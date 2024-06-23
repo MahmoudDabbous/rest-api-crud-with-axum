e11dd7788382b785eeb614734ff4ee9ac55b5c4e
Mahmoud Osama Dabbous <109554499+MahmoudDabbous@users.noreply.github.com>
2024-06-24 00:42:22 +0300

    refactor: used query macros in handlers

--- src/handlers.rs
+++ src/handlers.rs
@@ -14,10 +14,17 @@ use crate::{
 pub async fn get_notes(
     state: State<Arc<AppState>>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let notes = sqlx::query_as::<_, NoteRow>("SELECT * FROM notes")
-        .fetch_all(&state.db)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    let notes = sqlx::query_as!(
+        NoteRow,
+        "
+            SELECT id, title, content
+            FROM notes
+        "
+    )
+    .fetch_all(&state.db)
+    .await
+    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+
     Ok((StatusCode::OK, Json(notes)))
 }
 
@@ -25,12 +32,18 @@ pub async fn create_note(
     state: State<Arc<AppState>>,
     Json(payload): Json<CreateNote>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let result = sqlx::query("INSERT INTO notes (title, content) VALUES (?, ?)")
-        .bind(&payload.title)
-        .bind(&payload.content)
-        .execute(&state.db)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
+    let result = sqlx::query_as!(
+        NoteRow,
+        "
+            INSERT INTO notes (title, content)
+            VALUES (?, ?)
+        ",
+        payload.title,
+        payload.content
+    )
+    .execute(&state.db)
+    .await
+    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
     Ok((
         StatusCode::CREATED,
@@ -42,11 +55,18 @@ pub async fn get_note_by_id(
     Path(id): Path<i32>,
     state: State<Arc<AppState>>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let note = sqlx::query_as::<_, NoteRow>("SELECT * FROM notes WHERE id = ?")
-        .bind(id)
-        .fetch_one(&state.db)
-        .await
-        .map_err(|e| (StatusCode::NOT_FOUND, e.to_string()))?;
+    let note = sqlx::query_as!(
+        NoteRow,
+        "
+            SELECT id, title, content
+            FROM notes
+            WHERE id = ?
+        ",
+        id
+    )
+    .fetch_one(&state.db)
+    .await
+    .map_err(|_| (StatusCode::NOT_FOUND, "Note not found".to_string()))?;
 
     Ok((StatusCode::OK, Json(note)))
 }
@@ -56,35 +76,38 @@ pub async fn update_note(
     state: State<Arc<AppState>>,
     Json(payload): Json<UpdateNote>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let row = sqlx::query_as::<_, NoteRow>("SELECT * FROM notes WHERE id = ?")
-        .bind(id)
-        .fetch_one(&state.db)
-        .await
-        .map_err(|_| (StatusCode::NOT_FOUND, "Note not found".to_string()))?;
+    let result = sqlx::query_as!(
+        NoteRow,
+        "
+            UPDATE notes
+            SET title = ?, content = ?
+            WHERE id = ?
+        ",
+        payload.title,
+        payload.content,
+        id
+    )
+    .execute(&state.db)
+    .await
+    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
-    sqlx::query("UPDATE notes SET title = ?, content = ? WHERE id = ?")
-        .bind(&payload.title.unwrap_or(row.title.unwrap()))
-        .bind(&payload.content.unwrap_or(row.content.unwrap()))
-        .bind(id)
-        .execute(&state.db)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
-
-    Ok((StatusCode::OK, "Note updated".to_string()))
+    Ok((StatusCode::OK, Json(result.rows_affected())))
 }
 pub async fn delete_note(
     Path(id): Path<i32>,
     state: State<Arc<AppState>>,
 ) -> Result<impl IntoResponse, (StatusCode, String)> {
-    let result = sqlx::query("DELETE FROM notes WHERE id = ?")
-        .bind(id)
-        .execute(&state.db)
-        .await
-        .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
-
-    if result.rows_affected() == 0 {
-        return Err((StatusCode::NOT_FOUND, "Note not found".to_string()));
-    }
+    sqlx::query_as!(
+        NoteRow,
+        "
+            DELETE FROM notes
+            WHERE id = ?
+        ",
+        id
+    )
+    .execute(&state.db)
+    .await
+    .map_err(|e| (StatusCode::INTERNAL_SERVER_ERROR, e.to_string()))?;
 
     Ok((StatusCode::NO_CONTENT, "".to_string()))
 }
